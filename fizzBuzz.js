1f40360d90592f348043fec820b5195e4cce3de5
Benjamin Meece <bemeece@gmail.com>
2020-05-22 13:17:45 -0500

    jazz bazzed

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,30 +1,19 @@
-// My attempt at the  Fizz-Buzz challenge
-
-// PROBLEM STATEMENT
-// Write a short program that prints each number from 1 to 100 on a new line. 
-// * For each multiple of 3, print "Fizz" instead of the number. 
-// * For each multiple of 5, print "Buzz" instead of the number. 
-// * For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
-
-for(var fizzBuzz = 1, fizzBuzz = 1; fizzBuzz <= 100; fizzBuzz++){
+for (var fazzBazz = 1; fazzBazz <= 100; fazzBazz++) {
     var say = "";
-    if( fizzBuzz % 3 == 0){
-        say += "Fizz";
-    }
-    if( fizzBuzz % 5 = 0){
-        say += "Buzz";
-    }
-    if(say == ""){
-        say = fizzBuzz;
-    }    
-    // console.log(); // Use this to display the output
-    
-    console.log(say);
 
-    // Leverage the Modulus operator -> %
-    // Learn about the Modulus operator: 
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder
-    
-    // Hey, don't we need a FOR loop to count from 1 to 100?
-    // Yes, yes we do - let's start with that!
+    if (fazzBazz % 15 == 0) {
+        say += "JazzBazz";
+    }
+
+    else if (fazzBazz % 3 == 0) {
+        say += "Jazz";
+    }
+    else if (fazzBazz % 5 == 0) {
+        say += "Bazz";
+    }
+
+    else {
+        say = fazzBazz;
+    }
+    console.log(say);
 }
